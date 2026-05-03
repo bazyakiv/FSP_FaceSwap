3ab06120bf2da94f3622b75568818b83debb1933
Yakiv Bazyshyn <bbazyshyn@gmail.com>
2026-05-03 12:37:29 +0200

    Instead of using 4 points to calculate face transformation, we're using all the face shape points in order to calculate a precise transform matrix vector for the mask and overlay image

--- app_modules/faceswap.py
+++ app_modules/faceswap.py
@@ -13,12 +13,10 @@ FACE_OVAL = [
     54, 103, 67, 109
 ]
 
-FACE_KEYS = [
-    4,33,263, 10 # Nose Tip, Left/Right inner eye corner, chin
-]
 
 
-img_path = "faceswap_image\zelya_face.png";
+
+img_path = "faceswap_image\styopa.png";
 
 
 def normalize_image(array):
@@ -66,8 +64,7 @@ class FaceSwap:
             face_points.append((pos_x, pos_y)); #FACE OVAL POINTS;
 
     
-        key_facepoints = np.array([face_points[i] for i in FACE_KEYS]) # get the key  points
-
+     
 
         
     
@@ -90,9 +87,9 @@ class FaceSwap:
 
         
 
-        overlay_key_facepoints = np.array([overlay_face_points[i] for i in FACE_KEYS]) # get the key points
+     
         overlay_faceoutline = np.array([overlay_face_points[i] for i in FACE_OVAL]) # get all the positions from the oval points list
-
+        faceoutline = np.array([face_points[i] for i in FACE_OVAL]) # get all the positions from the oval points list
         
         
         mask = np.zeros((ov_h,ov_w), dtype=np.uint8) # create an empty mask
@@ -100,18 +97,18 @@ class FaceSwap:
         cv.fillConvexPoly(mask, overlay_faceoutline, 255); # create a polygon figure and fill it with white color, that will represent our mask
        
 
-        src_pts = np.float32([overlay_key_facepoints[0], overlay_key_facepoints[1], overlay_key_facepoints[2], overlay_key_facepoints[3]])
-        dst_pts = np.float32([key_facepoints[0], key_facepoints[1], key_facepoints[2],key_facepoints[3]])
+        src_pts = np.float32(overlay_faceoutline)
+        dst_pts = np.float32(faceoutline)
 
-        matrix = cv.getPerspectiveTransform(src_pts, dst_pts);
-        overlay_image = cv.warpPerspective(overlay_image, matrix, (w, h))
-        mask = cv.warpPerspective(mask, matrix, (w, h))
-        overlay_image = np.astype(overlay_image, np.float32)
-        mask = cv.blur(src=mask, ksize=(15,15)); # blur the edges for smooth transitions
-        normalized_mask = normalize_image(mask); # normalize from values from 0-255 to 0-1
-        normalized_mask = np.expand_dims(normalized_mask, axis=2); # add the channel axis to the mask, since it only has (h,w) but it needs (h,w,1)
+        
         try:
-           
+            [matrix, m_mask] = cv.findHomography(src_pts, dst_pts);
+            overlay_image = cv.warpPerspective(overlay_image, matrix, (w, h))
+            mask = cv.warpPerspective(mask, matrix, (w, h))
+            overlay_image = np.astype(overlay_image, np.float32)
+            mask = cv.blur(src=mask, ksize=(15,15)); # blur the edges for smooth transitions
+            normalized_mask = normalize_image(mask); # normalize from values from 0-255 to 0-1
+            normalized_mask = np.expand_dims(normalized_mask, axis=2); # add the channel axis to the mask, since it only has (h,w) but it needs (h,w,1)
 
             foreground = overlay_image * normalized_mask ; # cut out the target face to match the masks shape
             
